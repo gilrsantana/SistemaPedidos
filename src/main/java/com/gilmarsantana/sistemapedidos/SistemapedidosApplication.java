b76c09284fdb4b46c6b336f0c246a36016234d29
Gilmar Santana <g.r.santana@hotmail.com>
2020-11-05 17:39:02 -0300

    Created project

---
+++ src/main/java/com/gilmarsantana/sistemapedidos/SistemapedidosApplication.java
@@ -0,0 +1,13 @@
+package com.gilmarsantana.sistemapedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemapedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemapedidosApplication.class, args);
+	}
+
+}
